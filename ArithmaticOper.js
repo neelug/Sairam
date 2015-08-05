f109bffa0c40265abe0b9ad54d6c45bd0211d544
neelug <neelu1899@gmail.com>
2015-08-04 17:40:29 -0700

    Initial commit

---
+++ ArithmaticOper.js
@@ -0,0 +1,19 @@
+function multiply()
+{
+var firstnum=document.getElementById('first').value;
+var secondnum=document.getElementById('second').value;
+var Result=parseInt(firstnum)*parseInt(secondnum);
+alert(Result);
+//or
+//console.log("MULIPICATION  IS: "+Result);
+}
+
+function division()
+{
+var firstnum=document.getElementById('first').value;
+var secondnum=document.getElementById('second').value;
+var Result=parseInt(firstnum)/parseInt(secondnum);
+alert(Result);
+//or
+//console.log("DIVISION IS:" +Result);
+}
---
+++ leapyear.js
@@ -0,0 +1,16 @@
+
+function checkLeapYear() {
+	var year = document.checkYear.year.value;
+		if (year % 4===0)
+		{
+		alert(year + " year is a leap year.");
+	}
+	else
+	{
+		alert(year + " year is Not a leap year.");
+	}
+}
+
+
+
+
