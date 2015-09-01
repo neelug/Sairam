8b3b166a0b9c3b53762b0d1d9fe95417a2cb34e9
neelug <neelu1899@gmail.com>
2015-09-01 13:40:12 -0700

    Delete caplet.js

--- caplet.js
+++
@@ -1,7 +0,0 @@
-
-function myFunction()
- {
-    var str = document.getElementById("myText").value;
-    var res = str.charAt(2);
-    document.getElementById("demo").innerHTML = res.toUpperCase();
-}
